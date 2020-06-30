c5ae73f7d631fbcabab86d4729bfda82872d77f3
Rheden John Royo <rhedenjohn@hotmail.com>
2020-07-01 00:41:14 +0800

    Add project files.

---
+++ BlazorMovies/Client/Helpers/RepositoryInMemory.cs
@@ -0,0 +1,21 @@
+﻿using BlazorMovies.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class RepositoryInMemory : IRepository
+    {
+        public List<Movie> GetMovies()
+        {
+            return new List<Movie>()
+    {
+                new Movie(){Title = "Spider-Man: Far From Home", ReleaseDate = new DateTime(2019, 7, 2), Poster = "https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_UX182_CR0,0,182,268_AL_.jpg"},
+                new Movie(){Title = "Moana", ReleaseDate = new DateTime(2016, 11, 23), Poster="https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_UX182_CR0,0,182,268_AL_.jpg"},
+                new Movie(){Title = "Inception", ReleaseDate = new DateTime(2010, 7, 16), Poster="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg"}
+            };
+        }
+    }
+}
